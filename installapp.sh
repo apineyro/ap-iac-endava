c6a09441f79e049f32c140be7df7010d749f94c6
Andrés <apineyro@outlook.com>
2022-10-27 19:19:41 -0300

    Update installapp.sh

--- installapp.sh
+++ installapp.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "Starting installation" >> /tmp/log.txt
-sudo apt-get update $$ sudo apt-get upgrade
-sudo apt-get install -y git
-mkdir /tmp/installation
+echo "Starting installation"
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y git nodejs npm docker docker-compose nginx
+sudo mkdir /opt/app1
+sudo chmod 777 /opt/app1
+cd /opt/app1
+git clone https://github.com/Nexpeque/cicdworkshop.git
+cd cicdworkshop/
+sudo npm install
+sudo npm audit fix
+sudo npm start
